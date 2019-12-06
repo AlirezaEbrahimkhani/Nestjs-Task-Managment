6707319b9edf76c8d1570206155b682539e5006f
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2019-12-06 04:34:31 +0330

    get all tasks method added to service and controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,4 +6,9 @@ export class TasksController {
 
     constructor(private taskService : TasksService){}
 
+
+    @Get()
+    getAllTasks(){
+        return this.taskService.getAllTasks();        
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    private tasks = [];
+
+    getAllTasks(){
+        return this.tasks;
+    }
+
+}
