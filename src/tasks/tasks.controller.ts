05070db19c52fbe55ddfac76e1d110d0dd8ac731
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2019-12-08 01:31:45 +0330

    get task by id methods added to project and test in postman

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,6 +12,11 @@ export class TasksController {
     @Get()
     getAllTasks(): Task[]{
         return this.taskService.getAllTasks();        
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id : string) : Task{
+        return this.taskService.getTaskById(id);
     }
 
     @Post()
