7fc11838ec4a4397ed453a47f345d805a8f2ff91
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2019-12-08 01:23:45 +0330

    adding create task dto to project and update methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -14,10 +15,7 @@ export class TasksController {
     }
 
     @Post()
-    createTask(
-        @Body('title') title : string ,
-        @Body('description') description : string
-    ): Task{
-        return this.taskService.createTask(title , description);
+    createTask(@Body() createTaskDto : CreateTaskDto): Task{
+        return this.taskService.createTask(createTaskDto);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import * as uuid from 'uuid/v1';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,7 +12,10 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title : string , description : string) : Task{
+    createTask(createTaskDto : CreateTaskDto) : Task{
+
+        const {title , description} = createTaskDto;
+
         const task: Task = {
             id : uuid(),
             title,
