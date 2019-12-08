5935c99968f2355c8d1881889c723a6a1681ac1f
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2019-12-09 00:38:41 +0330

    is not empty validation attach to create task dto prop. and add ValidationPip to createTask method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -25,6 +25,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto : CreateTaskDto): Task{
         return this.taskService.createTask(createTaskDto);
     }
