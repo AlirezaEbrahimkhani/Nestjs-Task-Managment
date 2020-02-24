230f4e0c0663ae828d7785f95ba197f026faca37
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-24 22:45:59 +0330

    create task method implemented

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -26,11 +26,11 @@ export class TasksController {
         return this.taskService.getTaskById(id);
     }
 
-    // @Post()
-    // @UsePipes(ValidationPipe)
-    // createTask(@Body() createTaskDto : CreateTaskDto): Task{
-    //     return this.taskService.createTask(createTaskDto);
-    // }
+    @Post()
+    @UsePipes(ValidationPipe)
+    createTask(@Body() createTaskDto : CreateTaskDto): Promise<Task>{
+        return this.taskService.createTask(createTaskDto);
+    }
 
     // @Delete('/:id') 
     // deleteTask(@Param('id') id : string): void{ 
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,6 +6,7 @@ import { GetFilterTaskDto } from './dto/get-tasks-filter.dto';
 import { TaskRepository } from './task.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
  
 @Injectable()
 export class TasksService {
@@ -25,6 +26,10 @@ export class TasksService {
         }
 
         return found;
+    }
+
+    async createTask(createTaskDto : CreateTaskDto) : Promise<Task>{
+        return this.taskRepository.createTask(createTaskDto);
     }
 
 
