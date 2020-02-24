b9692a616f312184e55451fe40924c864a8f80d5
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-24 23:20:11 +0330

    remove comments

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,8 +11,6 @@ import { TaskStatus } from './task-status.enum';
 @Injectable()
 export class TasksService {
 
-    //--------------------------------------------------- Start API with Database ---------------------------------------------------//
-
     constructor(
         @InjectRepository(TaskRepository)
         private taskRepository: TaskRepository
@@ -54,70 +52,5 @@ export class TasksService {
         return task;
 
     }
- 
-
-    //--------------------------------------------------- Finish API with Database ---------------------------------------------------//
-
-
-    //--------------------------------------------------- Start API with array ---------------------------------------------------//
-
-    // private tasks : Task[] = [];
-
-    // getAllTasks(): Task[]{
-    //     return this.tasks;
-    // }
-
-    // getTaskWithFilters(filterDto : GetFilterTaskDto): Task[]{
-    //     const { status , search } = filterDto;
-    //     let tasks = this.getAllTasks();
-    //     if(status){
-    //         tasks = tasks.filter((task) => task.status === status);
-    //     }
-
-    //     if(search){
-    //         tasks = tasks.filter(task =>
-    //             task.title.includes(search) || 
-    //             task.description.includes(search)
-    //         );
-    //     }
-    //     return tasks;
-    // }
-
-    // getTaskById(id : string):Task{
-    //     const found =  this.tasks.find((task) => task.id === id);
-
-        // if(!found){
-        //     throw new NotFoundException(`Task with id "${id}" not found`);
-        // }
-
-    //     return found;
-    // }
-
-    // createTask(createTaskDto : CreateTaskDto) : Task{
-
-    //     const {title , description} = createTaskDto;
-
-    //     const task: Task = {
-    //         id : uuid(),
-    //         title,
-    //         description,
-    //         status : TaskStatus.OPEN
-    //     }
-    //     this.tasks.push(task);
-    //     return task;
-    // }
-
-    // deleteTask(id : string): void{
-    //     const found = this.getTaskById(id);
-    //     this.tasks = this.tasks.filter((task) => task.id != found.id); 
-    // } 
-
-    // updateTask(id : string , taskStatus : TaskStatus): Task{
-    //     const task = this.getTaskById(id);
-    //     task.status = taskStatus;
-    //     return task;
-    // }
-
-    //--------------------------------------------------- Finish API with array ---------------------------------------------------//
 
 }
