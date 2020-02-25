b5a5267e17e90ee696236dcd38a1babbed5cf1b4
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-26 01:45:29 +0330

    user repository created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository : UserRepository
+    ){}
+
+}
