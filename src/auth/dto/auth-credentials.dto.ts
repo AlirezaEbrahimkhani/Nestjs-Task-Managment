5f65c62f49bc41f18b36f5e2a978e23bc8cc62ab
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-26 11:29:36 +0330

    add some validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,6 +1,19 @@
+import { IsString, MinLength, MaxLength, Matches } from "class-validator";
+
 export class AuthCredentialsDto {
 
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username : string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(
+        /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/ ,
+        {message : 'Password too weak !'}
+    )
     password :string;
 
 }
