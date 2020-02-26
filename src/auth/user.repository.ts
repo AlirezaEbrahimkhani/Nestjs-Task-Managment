c53fd40b0c98d714ce9a8616120781ce629d8a48
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-26 11:50:29 +0330

    hash password using bcrypt methods

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,5 @@
 import { Repository, EntityRepository } from "typeorm";
+import * as bcrypt from 'bcrypt'
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
@@ -12,7 +13,8 @@ export class UserRepository extends Repository<User>{
 
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt();;
+        user.password = await this.hashPassword(password , user.salt);
 
         try {
             await user.save();
@@ -23,6 +25,10 @@ export class UserRepository extends Repository<User>{
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    private async hashPassword(password : string , salt : string) : Promise<string>{
+        return bcrypt.hash(password , salt);
     }
 
 }
