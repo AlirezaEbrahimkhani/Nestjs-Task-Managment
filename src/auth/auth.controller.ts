9e5d21a005bc7be81edc085dbbd27dae9d42d801
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-26 13:32:23 +0330

    import jwt strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, ValidationPipe, UnauthorizedException } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
