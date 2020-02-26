c11588639b3f8446b84824fd224c4cc61cd5dfa3
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-02-26 22:34:05 +0330

    add some log

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, Logger } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,6 +13,8 @@ import { GetUser } from 'src/auth/get-user.decorator';
 @UseGuards(AuthGuard()) 
 export class TasksController {
 
+    private logger = new Logger('TasksController');
+
     constructor(private taskService : TasksService){}
 
     @Get()
@@ -20,6 +22,7 @@ export class TasksController {
         @Query(ValidationPipe) filterDto : GetFilterTaskDto,
         @GetUser() user : User
     ) : Promise<Task[]>{
+        this.logger.verbose(`User "${user.username}" retreving all the tasks. Filters : ${JSON.stringify(filterDto)}`)
        return this.taskService.getTasks(filterDto , user);     
     }
 
@@ -34,6 +37,7 @@ export class TasksController {
         @Body() createTaskDto : CreateTaskDto,
         @GetUser() user : User
     ): Promise<Task>{
+        this.logger.verbose(`User "${user.username}" creating a new Task. Data : ${JSON.stringify(createTaskDto)}`)
         return this.taskService.createTask(createTaskDto , user);
     }
 
